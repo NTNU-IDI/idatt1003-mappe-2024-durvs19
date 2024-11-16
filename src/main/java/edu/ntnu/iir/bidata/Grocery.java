2c80534fd0397cac7400ba81a155e47a1518e48e
Durva <durvaparmar19@gmail.com>
2024-11-16 14:52:41 +0100

    created grocery class and validated the inputs in constructor as well as used this. (referance variable) to access instance variables of the grocery class

---
+++ src/main/java/edu/ntnu/iir/bidata/Grocery.java
@@ -0,0 +1,42 @@
+package edu.ntnu.iir.bidata;
+
+import java.util.Date;
+
+public class Grocery {
+
+  private String name;
+  private double quantity;
+  private String unit; // grams, litres, pcs, etc.
+  private Date bestBefore;
+  private double PriceperUnit; // so its easier to calculate price of different quantities
+
+  // create a constructor for this
+  public Grocery(String name, double quantity, String unit, Date bestBefore, double PriceperUnit) {
+
+    //validate inputs in constructor
+    if (name == null || name.isEmpty()) {
+      throw new IllegalArgumentException("Name cannot be null or empty");
+    }
+    if (quantity <= 0) {
+      throw new IllegalArgumentException("Quantity must be greater than 0");
+    }
+    if (unit == null || unit.isEmpty()) {
+      throw new IllegalArgumentException("Unit cannot be null or empty");
+    }
+    if (bestBefore == null) {
+      throw new IllegalArgumentException("Best before cannot be null");
+    }
+    if (PriceperUnit <= 0) {
+      throw new IllegalArgumentException("Price per unit must be greater than 0");
+    }
+    // reference variable used to access instance variables and methods of the current object
+    this.name = name;
+    this.quantity = quantity;
+    this.unit = unit;
+    this.bestBefore = bestBefore;
+    this.PriceperUnit = PriceperUnit;
+
+  }
+
+
+}

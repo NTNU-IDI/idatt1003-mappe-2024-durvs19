d52b24a2fcef1a0974c43e4b822c506db1ffbcea
Durva <durvaparmar19@gmail.com>
2024-11-22 01:30:37 +0100

    changed assertEquals() method from JUnit 4 to JUnit 5's assertEquals() method

--- src/test/java/GroceryTest.java
+++ src/test/java/GroceryTest.java
@@ -2,6 +2,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 
 import edu.ntnu.iir.bidata.Grocery;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
 
@@ -10,11 +11,12 @@ class GroceryTest {
   @Test
   void testConstructorValidInputs() {
     Grocery grocery = new Grocery("Milk", 2.0, "liters", 15.0, LocalDate.of(2023, 12, 31));
-    assertEquals("Milk", grocery.getName());
-    assertEquals(2.0, grocery.getQuantity());
-    assertEquals("liters", grocery.getUnit());
-    assertEquals(15.0, grocery.getPricePerUnit());
-    assertEquals(LocalDate.of(2023, 12, 31), grocery.getExpiryDate());
+   //replaced assertEquals with Assertions.assertEquals bcs of junit5
+    Assertions.assertEquals("Milk", grocery.getName());
+    Assertions.assertEquals(2.0, grocery.getQuantity());
+    Assertions.assertEquals("liters", grocery.getUnit());
+    Assertions.assertEquals(15.0, grocery.getPricePerUnit());
+    Assertions.assertEquals(LocalDate.of(2023, 12, 31), grocery.getExpiryDate());
   }
 
   @Test
@@ -37,9 +39,9 @@ class GroceryTest {
 
   @Test
   void testSetQuantityValid() {
-    Grocery grocery = new Grocery("Milk", 2.0, "liters", 15.0, LocalDate.of(2023, 12, 31));
+    Grocery grocery = new Grocery("Milk", 2, "liters", 15.0, LocalDate.of(2023, 12, 31));
     grocery.setQuantity(3.0);
-    assertEquals(3.0, grocery.getQuantity());
+    Assertions.assertEquals(3.0, grocery.getQuantity());
   }
 
   @Test

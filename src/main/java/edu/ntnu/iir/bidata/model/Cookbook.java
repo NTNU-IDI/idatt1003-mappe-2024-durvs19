5472e957e69ba7f1e3b452961e6032a000ac1418
Durva <durvaparmar19@gmail.com>
2024-11-24 17:53:34 +0100

    added a recipe and cookbook class

---
+++ src/main/java/edu/ntnu/iir/bidata/model/Cookbook.java
@@ -0,0 +1,24 @@
+package edu.ntnu.iir.bidata.model;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Data;
+
+/**
+ * The {@code Cookbook} class represents a collection of recipes. It provides methods to manage the
+ * list of recipes.
+ *
+ * <p>Example usage:
+ *
+ * <pre>
+ * Cookbook cookbook = new Cookbook();
+ * cookbook.getRecipes().add(new Recipe("Spaghetti Bolognese"));
+ * </pre>
+ */
+@Data
+public class Cookbook {
+
+  /** A list of recipes contained in the cookbook. */
+  private List<Recipe> recipes = new ArrayList<>();
+}

6c6e948c500bff8d0604f39773a1a435bc895b89
Durva <durvaparmar19@gmail.com>
2024-11-28 14:13:10 +0100

    javadoc comment

--- src/main/java/edu/ntnu/iir/bidata/services/GroceryService.java
+++ src/main/java/edu/ntnu/iir/bidata/services/GroceryService.java
@@ -15,7 +15,7 @@ public class GroceryService {
    *
    * @param grocery the grocery item to check
    * @return {@code true} if the grocery item is expired, {@code false} otherwise
-   * @example Grocery grocery = new Grocery(); grocery.setExpiryDate(LocalDate.of(2021, 10, 1));
+   * {@code @example} Grocery grocery = new Grocery(); grocery.setExpiryDate(LocalDate.of(2021, 10, 1));
    *        boolean expired = groceryService.isExpired(grocery);
    */
   public boolean isExpired(Grocery grocery) {
@@ -27,7 +27,7 @@ public class GroceryService {
    *
    * @param grocery the grocery item to calculate the value for
    * @return the total value of the grocery item
-   * @example Grocery grocery = new Grocery(); grocery.setQuantity(10);
+   * {@code @example} Grocery grocery = new Grocery(); grocery.setQuantity(10);
    *        grocery.setPricePerUnit(2.5); double value = groceryService.calculateValue(grocery);
    */
   public double calculateValue(Grocery grocery) {
@@ -40,7 +40,7 @@ public class GroceryService {
    * @param existingGrocery   the existing grocery item
    * @param newlyAddedGrocery the newly added grocery item
    * @return {@code true} if the two grocery items can be clubbed together, {@code false} otherwise
-   * @example Grocery grocery1 = new Grocery(); grocery1.setName("Apple"); grocery1.setUnit("kg");
+   * {@code @example} Grocery grocery1 = new Grocery(); grocery1.setName("Apple"); grocery1.setUnit("kg");
    *        grocery1.setPricePerUnit(3.0); grocery1.setExpiryDate(LocalDate.of(2023, 10, 1));
    *        Grocery grocery2 = new Grocery(); grocery2.setName("Apple"); grocery2.setUnit("kg");
    *        grocery2.setPricePerUnit(3.0); grocery2.setExpiryDate(LocalDate.of(2023, 10, 1));
